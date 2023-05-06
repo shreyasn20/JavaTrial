f7ccffd22dfa73d2ee1ff110780c859c9a2fa41b
shreyasn20 <shreyasnimbhore17@gmail.com>
2023-05-06 15:06:50 +0530

    Method added by dev1

--- PWFirstProgram/src/pwpkg1/PwHello.java
+++ PWFirstProgram/src/pwpkg1/PwHello.java
@@ -2,6 +2,9 @@ package pwpkg1;
 
 public class PwHello {
 
+	public void change1(){
+		System.out.println("Dev1 did changes");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
