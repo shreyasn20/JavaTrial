45b2127cc85e5571b25ecbc0c1aad75e671b8c3b
shreyasn20 <shreyasnimbhore17@gmail.com>
2023-05-06 16:09:50 +0530

    exp change

---
+++ PWFirstProgram/src/pwpkg1/exp.java.java
@@ -0,0 +1,13 @@
+package pwpkg1;
+
+public class PwHello {
+
+	public void change1(){
+		System.out.println("Dev1 did changes");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+
+}
